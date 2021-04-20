21898a1bfe56c7ba761f754ac54ba17837ec1c61
Brian Conway <bconway@rcesoftware.com>
2021-04-20 16:36:27 -0500

    Add App service to API
    - Add App service, unit and integration tests. Update API struct.
    - Sync `vendor/` for updated dependencies.
    - All test variations pass.

--- go.mod
+++ go.mod
@@ -14,11 +14,13 @@ require (
 	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
 	github.com/jackc/pgx/v4 v4.11.0
 	github.com/mennanov/fmutils v0.1.0
-	github.com/ownmfa/api/go v0.0.0-20210420174205-414ecb51f1db
+	github.com/ownmfa/api/go v0.0.0-20210420213335-c9bea88749d4
 	github.com/rs/zerolog v1.21.0
 	github.com/smira/go-statsd v1.3.2
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210415154028-4f45737414dc
+	golang.org/x/net v0.0.0-20210420210106-798c2154c571 // indirect
+	golang.org/x/sys v0.0.0-20210420205809-ac73e9fd8988 // indirect
 	google.golang.org/grpc v1.37.0
 	google.golang.org/protobuf v1.26.0
 )
---
+++ internal/api/test/app_test.go
@@ -0,0 +1,571 @@
+// +build !unit
+
+package test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/ownmfa/api/go/api"
+	"github.com/ownmfa/hermes/pkg/test/random"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+)
+
+func TestCreateApp(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Create valid app", func(t *testing.T) {
+		t.Parallel()
+
+		app := random.App("api-app", uuid.NewString())
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		createApp, err := appCli.CreateApp(ctx,
+			&api.CreateAppRequest{App: app})
+		t.Logf("createApp, err: %+v, %v", createApp, err)
+		require.NoError(t, err)
+		require.NotEqual(t, app.Id, createApp.Id)
+		require.WithinDuration(t, time.Now(), createApp.CreatedAt.AsTime(),
+			2*time.Second)
+		require.WithinDuration(t, time.Now(), createApp.UpdatedAt.AsTime(),
+			2*time.Second)
+	})
+
+	t.Run("Create valid app with insufficient role", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(secondaryViewerGRPCConn)
+		createApp, err := appCli.CreateApp(ctx, &api.CreateAppRequest{
+			App: random.App("api-app", uuid.NewString()),
+		})
+		t.Logf("createApp, err: %+v, %v", createApp, err)
+		require.Nil(t, createApp)
+		require.EqualError(t, err, "rpc error: code = PermissionDenied desc = "+
+			"permission denied, ADMIN role required")
+	})
+
+	t.Run("Create invalid app", func(t *testing.T) {
+		t.Parallel()
+
+		app := random.App("api-app", uuid.NewString())
+		app.Name = "api-app-" + random.String(40)
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		createApp, err := appCli.CreateApp(ctx,
+			&api.CreateAppRequest{App: app})
+		t.Logf("createApp, err: %+v, %v", createApp, err)
+		require.Nil(t, createApp)
+		require.EqualError(t, err, "rpc error: code = InvalidArgument desc = "+
+			"invalid CreateAppRequest.App: embedded message failed validation "+
+			"| caused by: invalid App.Name: value length must be between 5 "+
+			"and 40 runes, inclusive")
+	})
+}
+
+func TestGetApp(t *testing.T) {
+	t.Parallel()
+
+	app := random.App("api-app", uuid.NewString())
+
+	ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+	defer cancel()
+
+	appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+	createApp, err := appCli.CreateApp(ctx,
+		&api.CreateAppRequest{App: app})
+	t.Logf("createApp, err: %+v, %v", createApp, err)
+	require.NoError(t, err)
+
+	t.Run("Get app by valid ID", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		getApp, err := appCli.GetApp(ctx,
+			&api.GetAppRequest{Id: createApp.Id})
+		t.Logf("getApp, err: %+v, %v", getApp, err)
+		require.NoError(t, err)
+
+		// Testify does not currently support protobuf equality:
+		// https://github.com/stretchr/testify/issues/758
+		if !proto.Equal(createApp, getApp) {
+			t.Fatalf("\nExpect: %+v\nActual: %+v", createApp, getApp)
+		}
+	})
+
+	t.Run("Get app by unknown ID", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		getApp, err := appCli.GetApp(ctx,
+			&api.GetAppRequest{Id: uuid.NewString()})
+		t.Logf("getApp, err: %+v, %v", getApp, err)
+		require.Nil(t, getApp)
+		require.EqualError(t, err, "rpc error: code = NotFound desc = object "+
+			"not found")
+	})
+
+	t.Run("Gets are isolated by org ID", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		secCli := api.NewAppServiceClient(secondaryAdminGRPCConn)
+		getApp, err := secCli.GetApp(ctx,
+			&api.GetAppRequest{Id: createApp.Id})
+		t.Logf("getApp, err: %+v, %v", getApp, err)
+		require.Nil(t, getApp)
+		require.EqualError(t, err, "rpc error: code = NotFound desc = object "+
+			"not found")
+	})
+}
+
+func TestUpdateApp(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Update app by valid app", func(t *testing.T) {
+		t.Parallel()
+
+		app := random.App("api-app", uuid.NewString())
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		createApp, err := appCli.CreateApp(ctx,
+			&api.CreateAppRequest{App: app})
+		t.Logf("createApp, err: %+v, %v", createApp, err)
+		require.NoError(t, err)
+
+		// Update app fields.
+		createApp.Name = "api-app-" + random.String(10)
+
+		updateApp, err := appCli.UpdateApp(ctx,
+			&api.UpdateAppRequest{App: createApp})
+		t.Logf("updateApp, err: %+v, %v", updateApp, err)
+		require.NoError(t, err)
+		require.Equal(t, createApp.Name, updateApp.Name)
+		require.True(t, updateApp.UpdatedAt.AsTime().After(
+			updateApp.CreatedAt.AsTime()))
+		require.WithinDuration(t, createApp.CreatedAt.AsTime(),
+			updateApp.UpdatedAt.AsTime(), 2*time.Second)
+
+		getApp, err := appCli.GetApp(ctx,
+			&api.GetAppRequest{Id: createApp.Id})
+		t.Logf("getApp, err: %+v, %v", getApp, err)
+		require.NoError(t, err)
+
+		// Testify does not currently support protobuf equality:
+		// https://github.com/stretchr/testify/issues/758
+		if !proto.Equal(updateApp, getApp) {
+			t.Fatalf("\nExpect: %+v\nActual: %+v", updateApp, getApp)
+		}
+	})
+
+	t.Run("Partial update app by valid app", func(t *testing.T) {
+		t.Parallel()
+
+		app := random.App("api-app", uuid.NewString())
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminKeyGRPCConn)
+		createApp, err := appCli.CreateApp(ctx,
+			&api.CreateAppRequest{App: app})
+		t.Logf("createApp, err: %+v, %v", createApp, err)
+		require.NoError(t, err)
+
+		// Update app fields.
+		part := &api.App{Id: createApp.Id, Name: "api-app-" + random.String(10)}
+
+		updateApp, err := appCli.UpdateApp(ctx, &api.UpdateAppRequest{
+			App: part, UpdateMask: &fieldmaskpb.FieldMask{Paths: []string{
+				"name",
+			}},
+		})
+		t.Logf("updateApp, err: %+v, %v", updateApp, err)
+		require.NoError(t, err)
+		require.Equal(t, part.Name, updateApp.Name)
+		require.True(t, updateApp.UpdatedAt.AsTime().After(
+			updateApp.CreatedAt.AsTime()))
+		require.WithinDuration(t, createApp.CreatedAt.AsTime(),
+			updateApp.UpdatedAt.AsTime(), 2*time.Second)
+
+		getApp, err := appCli.GetApp(ctx,
+			&api.GetAppRequest{Id: createApp.Id})
+		t.Logf("getApp, err: %+v, %v", getApp, err)
+		require.NoError(t, err)
+
+		// Testify does not currently support protobuf equality:
+		// https://github.com/stretchr/testify/issues/758
+		if !proto.Equal(updateApp, getApp) {
+			t.Fatalf("\nExpect: %+v\nActual: %+v", updateApp, getApp)
+		}
+	})
+
+	t.Run("Update nil app", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		updateApp, err := appCli.UpdateApp(ctx,
+			&api.UpdateAppRequest{App: nil})
+		t.Logf("updateApp, err: %+v, %v", updateApp, err)
+		require.Nil(t, updateApp)
+		require.EqualError(t, err, "rpc error: code = InvalidArgument desc = "+
+			"invalid UpdateAppRequest.App: value is required")
+	})
+
+	t.Run("Update app with insufficient role", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(secondaryViewerGRPCConn)
+		updateApp, err := appCli.UpdateApp(ctx, &api.UpdateAppRequest{
+			App: random.App("api-app", uuid.NewString()),
+		})
+		t.Logf("updateApp, err: %+v, %v", updateApp, err)
+		require.Nil(t, updateApp)
+		require.EqualError(t, err, "rpc error: code = PermissionDenied desc = "+
+			"permission denied, ADMIN role required")
+	})
+
+	t.Run("Update app with insufficient key role", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(secondaryViewerKeyGRPCConn)
+		updateApp, err := appCli.UpdateApp(ctx, &api.UpdateAppRequest{
+			App: random.App("api-app", uuid.NewString()),
+		})
+		t.Logf("updateApp, err: %+v, %v", updateApp, err)
+		require.Nil(t, updateApp)
+		require.EqualError(t, err, "rpc error: code = PermissionDenied desc = "+
+			"permission denied, ADMIN role required")
+	})
+
+	t.Run("Partial update invalid field mask", func(t *testing.T) {
+		t.Parallel()
+
+		app := random.App("api-app", uuid.NewString())
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		updateApp, err := appCli.UpdateApp(ctx, &api.UpdateAppRequest{
+			App: app, UpdateMask: &fieldmaskpb.FieldMask{
+				Paths: []string{"aaa"},
+			},
+		})
+		t.Logf("updateApp, err: %+v, %v", updateApp, err)
+		require.Nil(t, updateApp)
+		require.EqualError(t, err, "rpc error: code = InvalidArgument desc = "+
+			"invalid field mask")
+	})
+
+	t.Run("Partial update app by unknown app", func(t *testing.T) {
+		t.Parallel()
+
+		app := random.App("api-app", uuid.NewString())
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		updateApp, err := appCli.UpdateApp(ctx, &api.UpdateAppRequest{
+			App: app, UpdateMask: &fieldmaskpb.FieldMask{
+				Paths: []string{"name"},
+			},
+		})
+		t.Logf("updateApp, err: %+v, %v", updateApp, err)
+		require.Nil(t, updateApp)
+		require.EqualError(t, err, "rpc error: code = NotFound desc = object "+
+			"not found")
+	})
+
+	t.Run("Update app by unknown app", func(t *testing.T) {
+		t.Parallel()
+
+		app := random.App("api-app", uuid.NewString())
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		updateApp, err := appCli.UpdateApp(ctx,
+			&api.UpdateAppRequest{App: app})
+		t.Logf("updateApp, err: %+v, %v", updateApp, err)
+		require.Nil(t, updateApp)
+		require.EqualError(t, err, "rpc error: code = NotFound desc = object "+
+			"not found")
+	})
+
+	t.Run("Updates are isolated by org ID", func(t *testing.T) {
+		t.Parallel()
+
+		app := random.App("api-app", uuid.NewString())
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		createApp, err := appCli.CreateApp(ctx,
+			&api.CreateAppRequest{App: app})
+		t.Logf("createApp, err: %+v, %v", createApp, err)
+		require.NoError(t, err)
+
+		// Update app fields.
+		createApp.OrgId = uuid.NewString()
+		createApp.Name = "api-app-" + random.String(10)
+
+		secCli := api.NewAppServiceClient(secondaryAdminGRPCConn)
+		updateApp, err := secCli.UpdateApp(ctx,
+			&api.UpdateAppRequest{App: createApp})
+		t.Logf("updateApp, err: %+v, %v", updateApp, err)
+		require.Nil(t, updateApp)
+		require.EqualError(t, err, "rpc error: code = NotFound desc = object "+
+			"not found")
+	})
+
+	t.Run("Update app validation failure", func(t *testing.T) {
+		t.Parallel()
+
+		app := random.App("api-app", uuid.NewString())
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		createApp, err := appCli.CreateApp(ctx,
+			&api.CreateAppRequest{App: app})
+		t.Logf("createApp, err: %+v, %v", createApp, err)
+		require.NoError(t, err)
+
+		// Update app fields.
+		createApp.Name = "api-app-" + random.String(40)
+
+		updateApp, err := appCli.UpdateApp(ctx,
+			&api.UpdateAppRequest{App: createApp})
+		t.Logf("updateApp, err: %+v, %v", updateApp, err)
+		require.Nil(t, updateApp)
+		require.EqualError(t, err, "rpc error: code = InvalidArgument desc = "+
+			"invalid UpdateAppRequest.App: embedded message failed validation "+
+			"| caused by: invalid App.Name: value length must be between 5 "+
+			"and 40 runes, inclusive")
+	})
+}
+
+func TestDeleteApp(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Delete app by valid ID", func(t *testing.T) {
+		t.Parallel()
+
+		app := random.App("api-app", uuid.NewString())
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		createApp, err := appCli.CreateApp(ctx,
+			&api.CreateAppRequest{App: app})
+		t.Logf("createApp, err: %+v, %v", createApp, err)
+		require.NoError(t, err)
+
+		_, err = appCli.DeleteApp(ctx,
+			&api.DeleteAppRequest{Id: createApp.Id})
+		t.Logf("err: %v", err)
+		require.NoError(t, err)
+
+		t.Run("Read app by deleted ID", func(t *testing.T) {
+			t.Parallel()
+
+			ctx, cancel := context.WithTimeout(context.Background(),
+				testTimeout)
+			defer cancel()
+
+			appCli := api.NewAppServiceClient(globalAdminKeyGRPCConn)
+			getApp, err := appCli.GetApp(ctx,
+				&api.GetAppRequest{Id: createApp.Id})
+			t.Logf("getApp, err: %+v, %v", getApp, err)
+			require.Nil(t, getApp)
+			require.EqualError(t, err, "rpc error: code = NotFound desc = "+
+				"object not found")
+		})
+	})
+
+	t.Run("Delete app with insufficient role", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(secondaryViewerGRPCConn)
+		_, err := appCli.DeleteApp(ctx,
+			&api.DeleteAppRequest{Id: uuid.NewString()})
+		t.Logf("err: %v", err)
+		require.EqualError(t, err, "rpc error: code = PermissionDenied "+
+			"desc = permission denied, ADMIN role required")
+	})
+
+	t.Run("Delete app by unknown ID", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		_, err := appCli.DeleteApp(ctx,
+			&api.DeleteAppRequest{Id: uuid.NewString()})
+		t.Logf("err: %v", err)
+		require.EqualError(t, err, "rpc error: code = NotFound desc = object "+
+			"not found")
+	})
+
+	t.Run("Deletes are isolated by org ID", func(t *testing.T) {
+		t.Parallel()
+
+		app := random.App("api-app", uuid.NewString())
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		createApp, err := appCli.CreateApp(ctx,
+			&api.CreateAppRequest{App: app})
+		t.Logf("createApp, err: %+v, %v", createApp, err)
+		require.NoError(t, err)
+
+		secCli := api.NewAppServiceClient(secondaryAdminGRPCConn)
+		_, err = secCli.DeleteApp(ctx,
+			&api.DeleteAppRequest{Id: createApp.Id})
+		t.Logf("err: %v", err)
+		require.EqualError(t, err, "rpc error: code = NotFound desc = object "+
+			"not found")
+	})
+}
+
+func TestListApps(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+	defer cancel()
+
+	appIDs := []string{}
+	appNames := []string{}
+	for i := 0; i < 3; i++ {
+		app := random.App("api-app", uuid.NewString())
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		createApp, err := appCli.CreateApp(ctx,
+			&api.CreateAppRequest{App: app})
+		t.Logf("createApp, err: %+v, %v", createApp, err)
+		require.NoError(t, err)
+
+		appIDs = append(appIDs, createApp.Id)
+		appNames = append(appNames, createApp.Name)
+	}
+
+	t.Run("List apps by valid org ID", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		listApps, err := appCli.ListApps(ctx, &api.ListAppsRequest{})
+		t.Logf("listApps, err: %+v, %v", listApps, err)
+		require.NoError(t, err)
+		require.GreaterOrEqual(t, len(listApps.Apps), 3)
+		require.GreaterOrEqual(t, listApps.TotalSize, int32(3))
+
+		var found bool
+		for _, app := range listApps.Apps {
+			if app.Id == appIDs[len(appIDs)-1] &&
+				app.Name == appNames[len(appNames)-1] {
+				found = true
+			}
+		}
+		require.True(t, found)
+	})
+
+	t.Run("List apps by valid org ID with next page", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminKeyGRPCConn)
+		listApps, err := appCli.ListApps(ctx,
+			&api.ListAppsRequest{PageSize: 2})
+		t.Logf("listApps, err: %+v, %v", listApps, err)
+		require.NoError(t, err)
+		require.Len(t, listApps.Apps, 2)
+		require.NotEmpty(t, listApps.NextPageToken)
+		require.GreaterOrEqual(t, listApps.TotalSize, int32(3))
+
+		nextApps, err := appCli.ListApps(ctx, &api.ListAppsRequest{
+			PageSize: 2, PageToken: listApps.NextPageToken,
+		})
+		t.Logf("nextApps, err: %+v, %v", nextApps, err)
+		require.NoError(t, err)
+		require.GreaterOrEqual(t, len(nextApps.Apps), 1)
+		require.GreaterOrEqual(t, nextApps.TotalSize, int32(3))
+	})
+
+	t.Run("Lists are isolated by org ID", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		secCli := api.NewAppServiceClient(secondaryAdminGRPCConn)
+		listApps, err := secCli.ListApps(ctx, &api.ListAppsRequest{})
+		t.Logf("listApps, err: %+v, %v", listApps, err)
+		require.NoError(t, err)
+		require.Len(t, listApps.Apps, 0)
+		require.Equal(t, int32(0), listApps.TotalSize)
+	})
+
+	t.Run("List apps by invalid page token", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(context.Background(), testTimeout)
+		defer cancel()
+
+		appCli := api.NewAppServiceClient(globalAdminGRPCConn)
+		listApps, err := appCli.ListApps(ctx,
+			&api.ListAppsRequest{PageToken: badUUID})
+		t.Logf("listApps, err: %+v, %v", listApps, err)
+		require.Nil(t, listApps)
+		require.EqualError(t, err, "rpc error: code = InvalidArgument desc = "+
+			"invalid page token")
+	})
+}
